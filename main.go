54467caa063eb9b8118a9bdd86b38aba0248b0cf
kidinamoto01 <kidinamoto@qq.com>
2018-04-01 22:06:58 +0800

    add address generator

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 //gaia init 后生成config.toml、genesis.json、priv_validator.json这三个文件
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
+
+	//"github.com/tendermint/go-crypto"
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/tendermint/go-crypto"
+)
+
+const(
+	password = "1234567890"
 )
 
 type Coin struct {
@@ -12,7 +20,7 @@ type Coin struct {
 }
 
 type Account struct{
-	Address string `json:"address"`;
+	Address crypto.Address  `json:"address"`;
 	Coins []Coin `json:"coins"`
 
 }
@@ -24,5 +32,57 @@ type AccountList struct {
 func main(){
 
 	//生成n个余额为100的账户
+
+
+	//num := 10
+	//accList := []Account{}
+	//
+	//
+	//
+	//bl := AccountList{
+	//	Accounts: []Account{
+	//		Account{},
+	//	},
+	//}
+	//data, _ := json.MarshalIndent(bl, "", "  ");
+	//fmt.Println(string(data));
+
+	account,seed := GenerateAccount("abc",password)
+	fmt.Println(account)
+	fmt.Println(seed)
+
+}
+
+
+
+func GenerateAccount(name string,pass string) (Account,string){
+
+	kb, err := keys.GetKeyBase() // dbm.NewMemDB()) // :(
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+
+	//var info cryptokeys.Info
+	info, seed, err := kb.Create(name, pass, "ed25519")
+
+	fmt.Println(seed)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fermion := Coin{Denom:"fermion",Amount:100}
+	coins :=[]Coin{fermion}
+	var add crypto.Address
+	add = info.Address()
+	account := Account{Address:add,Coins:coins}
+	fmt.Println(account)
+
+	return account,seed
+
+
 
 }
